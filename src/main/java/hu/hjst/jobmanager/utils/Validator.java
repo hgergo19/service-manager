e9f8c686e360a8a7f3d9beb5ce53a65aa1160fc8
Horváth Gergő <hgergo19@gmail.com>
2021-09-19 11:02:46 +0200

    Validator util class was added.

---
+++ src/main/java/hu/hjst/jobmanager/utils/Validator.java
@@ -0,0 +1,14 @@
+package hu.hjst.jobmanager.utils;
+
+import hu.hjst.jobmanager.models.dtos.CustomerCreateDto;
+
+public class Validator {
+
+  //TODO : overloaded constructors
+  public static void validate(CustomerCreateDto dto) {
+    String companyName = dto.getCompanyName();
+    if (companyName == null || companyName.isEmpty()) {
+      throw new IllegalArgumentException("Please fill the company name!");
+    }
+  }
+}
