7c13e9eb3ab19cdeb268c67bcf1f70bbca655003
Horváth Gergő <hgergo19@gmail.com>
2021-09-21 21:08:18 +0200

    New customer post endpoint skeleton was added.
    <Added and tested form data input (dto) to controller. >

--- src/main/java/hu/hjst/jobmanager/controllers/web/CustomerWebController.java
+++ src/main/java/hu/hjst/jobmanager/controllers/web/CustomerWebController.java
@@ -1,13 +1,28 @@
 package hu.hjst.jobmanager.controllers.web;
 
+import hu.hjst.jobmanager.models.dtos.CustomerCreateDto;
+import hu.hjst.jobmanager.services.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerWebController {
 
+  private CustomerService customerService;
+
+  public CustomerWebController(CustomerService customerService) {
+    this.customerService = customerService;
+  }
+
   @GetMapping("/new-customer")
   public String customerCreate() {
+    return "new-customer";
+  }
+
+  @PostMapping("new-customer")
+  public String addCustomer(CustomerCreateDto dto) {
+    System.out.println(dto);
     return "new-customer";
   }
 }
