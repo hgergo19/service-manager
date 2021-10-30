68120e47460e4452f6209bc46b6677a61e9d8a9a
Horvath Gergoe <hgergo19@gmail.com>
2021-10-30 16:59:01 +0200

    FindAll function was added to repo layer

--- src/main/java/hu/hjst/jobmanager/repositories/CustomerRepository.java
+++ src/main/java/hu/hjst/jobmanager/repositories/CustomerRepository.java
@@ -4,8 +4,12 @@ import hu.hjst.jobmanager.models.entities.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
   Boolean existsByCompanyNameIgnoreCase(String companyName);
+
+  List<Customer> findAll();
 }
