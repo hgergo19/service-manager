85c7b31f5bfaeb933c291b281f3227246ee9adb9
Horvath Gergoe <hgergo19@gmail.com>
2021-11-13 09:24:35 +0100

    Style update

--- src/main/java/hu/hjst/jobmanager/controllers/web/CustomerWebController.java
+++ src/main/java/hu/hjst/jobmanager/controllers/web/CustomerWebController.java
@@ -7,37 +7,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class CustomerWebController {
 
-  private CustomerService customerService;
+    private CustomerService customerService;
 
-  public CustomerWebController(CustomerService customerService) {
-    this.customerService = customerService;
-  }
+    public CustomerWebController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
-  @GetMapping("/customer-home")
-  public String customerCreate() {
-    return "new-customer-home";
-  }
+    @GetMapping("/customer-home")
+    public String customerCreate() {
+        return "new-customer-home";
+    }
 
-  @PostMapping("/new-customer")
-  public String addCustomer(CustomerCreateDto dto) {
-    customerService.newCustomer(dto);
-    return "redirect:/customers";
-  }
+    @PostMapping("/new-customer")
+    public String addCustomer(CustomerCreateDto dto) {
+        customerService.newCustomer(dto);
+        return "redirect:/customers";
+    }
 
-  @GetMapping("/new-customer")
-  public String addCustomer() {
-    return "new-customer";
-  }
+    @GetMapping("/new-customer")
+    public String addCustomer() {
+        return "new-customer";
+    }
 
-  @GetMapping("/customers")
-  public String listCustomers(Model model){
-    model.addAttribute("customers",customerService.listCustomers());
-    return "new-customers";
-  }
+    @GetMapping("/customers")
+    public String listCustomers(Model model) {
+        model.addAttribute("customers", customerService.listCustomers());
+        return "new-customers";
+    }
+
+    @PostMapping("/deleteCustomerById")
+    public String deleteCustomerById(@RequestParam("id") Long buyerId) {
+        customerService.deleteCustomerById(buyerId);
+        return "redirect:/customers";
+    }
 }
