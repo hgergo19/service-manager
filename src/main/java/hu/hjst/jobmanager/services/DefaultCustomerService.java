b4f9cb07f4c2cc2bfbda910c5e86efd4341b967e
Horvath Gergoe <hgergo19@gmail.com>
2021-11-13 09:24:31 +0100

    Style update

--- src/main/java/hu/hjst/jobmanager/services/DefaultCustomerService.java
+++ src/main/java/hu/hjst/jobmanager/services/DefaultCustomerService.java
@@ -15,55 +15,57 @@ import java.util.List;
 @Service
 public class DefaultCustomerService implements CustomerService {
 
-  private final ModelMapper modelMapper = new ModelMapper();
-  private final CustomerRepository repository;
+    private final ModelMapper modelMapper = new ModelMapper();
+    private final CustomerRepository repository;
 
-  @Autowired
-  public DefaultCustomerService(CustomerRepository repository) {
-    this.repository = repository;
-  }
-
-  @Override
-  public CustomerResponseDto newCustomer(CustomerCreateDto dto) {
-    Validator.validate(dto.getCompanyName(), "Please fill the company name field!");
-
-    if (repository.existsByCompanyNameIgnoreCase(dto.getCompanyName())) {
-      throw new IllegalArgumentException(
-          "Company already exists"); //TODO : controller level exception handling (later REST)/ error pages
+    @Autowired
+    public DefaultCustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
-    Customer customer = modelMapper.map(dto, Customer.class);
-    Customer savedCustomer = repository.save(customer);
-    return modelMapper.map(savedCustomer, CustomerResponseDto.class);
-  }
 
-  @Override
-  public CustomerResponseDto findCustomerById(Long id) {
-    return null;
-  }
+    @Override
+    public CustomerResponseDto newCustomer(CustomerCreateDto dto) {
+        Validator.validate(dto.getCompanyName(), "Please fill the company name field!");
 
-  @Override
-  public CustomerResponseDto findCustomerByName(String name) {
-    return null;
-  }
-
-  @Override
-  public List<CustomerResponseDto> listCustomers() {
-    modelMapper.getConfiguration().setAmbiguityIgnored(true);
-    List<CustomerResponseDto> response = new ArrayList<>();
-    List<Customer> customers = repository.findAll();
-    for (Customer customer : customers) {
-      response.add(modelMapper.map(customer,CustomerResponseDto.class));
+        if (repository.existsByCompanyNameIgnoreCase(dto.getCompanyName())) {
+            throw new IllegalArgumentException(
+                    "Company already exists"); //TODO : controller level exception handling (later REST)/ error pages
+        }
+        Customer customer = modelMapper.map(dto, Customer.class);
+        Customer savedCustomer = repository.save(customer);
+        return modelMapper.map(savedCustomer, CustomerResponseDto.class);
     }
-    System.out.println(response);
-    System.out.println(customers);
-    return response;
-  }
 
-  @Override
-  public void deleteCustomerById(Long id) {
-  }
+    @Override
+    public CustomerResponseDto findCustomerById(Long id) {
+        return null;
+    }
 
-  @Override
-  public void modifyCustomerById(Long id) {
-  }
+    @Override
+    public CustomerResponseDto findCustomerByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerResponseDto> listCustomers() {
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+        List<CustomerResponseDto> response = new ArrayList<>();
+        List<Customer> customers = repository.findAll();
+        for (Customer customer : customers) {
+            response.add(modelMapper.map(customer, CustomerResponseDto.class));
+        }
+        System.out.println(response);
+        System.out.println(customers);
+        return response;
+    }
+
+    @Override
+    public void deleteCustomerById(Long id) {
+        //Null validation ?
+        repository.deleteById(id);
+    }
+
+    @Override
+    public void modifyCustomerById(Long id) {
+    }
 }
