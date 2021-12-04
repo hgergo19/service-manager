a6702ea4d04eb417eae1e0d11e3a07b02f978fdb
Horvath Gergoe <hgergo19@gmail.com>
2021-12-04 12:47:11 +0100

    Validator was updated.

--- src/main/java/hu/hjst/jobmanager/utils/Validator.java
+++ src/main/java/hu/hjst/jobmanager/utils/Validator.java
@@ -1,21 +1,16 @@
 package hu.hjst.jobmanager.utils;
 
-import hu.hjst.jobmanager.models.dtos.CustomerCreateDto;
-
 public class Validator {
 
-  //TODO : overloaded constructors
-  public static void validate(CustomerCreateDto dto) {
-    String companyName = dto.getCompanyName();
-    if (companyName == null || companyName.isEmpty()) {
-      throw new IllegalArgumentException("Please fill the company name!");
+    public static void validate(String text, String message) {
+        if (text == null || text.isEmpty()) {
+            throw new IllegalArgumentException(message);
+        }
     }
-  }
 
-  public static void validate(String text, String message) {
-    if (text == null || text.isEmpty()) {
-      throw new IllegalArgumentException(message);
+    public static void validate(Object o, String message) {
+        if (o == null) {
+            throw new IllegalArgumentException(message);
+        }
     }
-  }
-
 }
