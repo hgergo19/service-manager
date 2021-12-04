04fd24c36476d36a00199f9d9c0028fd48b84ccd
Horvath Gergoe <hgergo19@gmail.com>
2021-12-04 12:25:19 +0100

    Interface for machines was added.

---
+++ src/main/java/hu/hjst/jobmanager/services/MachineService.java
@@ -0,0 +1,25 @@
+package hu.hjst.jobmanager.services;
+
+import hu.hjst.jobmanager.models.dtos.MachineCreateDto;
+import hu.hjst.jobmanager.models.dtos.MachineDto;
+
+import java.util.List;
+
+public interface MachineService {
+
+    MachineDto newMachine (MachineCreateDto dto);
+
+    MachineDto findMachinesBySerialNumber (String serialNumber);
+
+    List<MachineDto> findMachinesByType(String type);
+
+    List<MachineDto> findMachinesByCustomer(String customerName);
+
+    List<MachineDto> findAllMachines();
+
+    void deleteMachineBySerialNumber(String serialNumber);
+
+    void modifyMachineBySerialNumber(String serialNumber);
+
+
+}
