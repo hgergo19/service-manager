9a482a7a6d52e51f44fc6359bbf15d2ddd3781ec
Horvath Gergoe <hgergo19@gmail.com>
2021-12-04 14:31:50 +0100

    Customer Response DTO was expended with machines list

--- src/main/java/hu/hjst/jobmanager/models/dtos/CustomerResponseDto.java
+++ src/main/java/hu/hjst/jobmanager/models/dtos/CustomerResponseDto.java
@@ -1,8 +1,11 @@
 package hu.hjst.jobmanager.models.dtos;
 
+import hu.hjst.jobmanager.models.entities.Machine;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,4 +21,5 @@ public class CustomerResponseDto {
     private String vatNumber;
     private String accountNumber;
     private String note;
+    private List<Machine> machines; //TODO convert to DTO
 }
