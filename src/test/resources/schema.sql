826111e3545f4c03a3e782c7800e8a2e8da46430
Horvath Gergoe <hgergo19@gmail.com>
2021-12-10 12:02:13 +0100

    H2 schema.sql was added.
    <p>still needs expansions</p>

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS jobs
+(
+    `job_id`         BIGINT NOT NULL PRIMARY KEY,
+    `start_date`     DATE,
+    `end_date`       DATE,
+    `completed`      BOOLEAN,
+    `invoiced`       BOOLEAN,
+    `invoice_number` VARCHAR(20),
+    `note`           VARCHAR(255),
+    `machine_serial_number`  VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS machines
+(
+    `serial_number`        varchar(20) NOT NULL PRIMARY KEY,
+    `type`                 VARCHAR(10),
+    `control_type`         VARCHAR(20),
+    `machine_hours`        INT,
+    `spindle_hours`        INT,
+    `nc_software_version`  VARCHAR(15),
+    `plc_software_version` VARCHAR(15),
+    `created_at`           DATETIME,
+    `last_updated_at`      DATETIME
+);
+
+ALTER TABLE jobs
+    ADD CONSTRAINT machine_serial_number FOREIGN KEY (machine_serial_number) REFERENCES machines(serial_number) ;
