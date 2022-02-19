a80225af8b6b2a9ee8b15090d984af9f51a6dd8a
Horvath Gergoe <hgergo19@gmail.com>
2022-02-19 12:31:10 +0100

    List active jobs in repo was added

--- src/main/java/hu/hjst/jobmanager/repositories/JobRepository.java
+++ src/main/java/hu/hjst/jobmanager/repositories/JobRepository.java
@@ -1,6 +1,7 @@
 package hu.hjst.jobmanager.repositories;
 
 import hu.hjst.jobmanager.models.entities.Job;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,4 +11,7 @@ public interface JobRepository extends CrudRepository<Job, Long> {
     boolean existsByJobId(Long id);
 
     List<Job> findAll();
+
+    @Query("SELECT j FROM Job j WHERE j.isCompleted = FALSE")
+    List<Job> findAllActive ();
 }
