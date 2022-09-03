df470a88d742a287165df37a05fe71f59c2a8981
Horvath Gergoe <hgergo19@gmail.com>
2022-09-03 20:52:37 +0200

    POST skeleton for add new job selectiong a customer, was added.

--- src/main/java/hu/hjst/jobmanager/controllers/web/JobWebController.java
+++ src/main/java/hu/hjst/jobmanager/controllers/web/JobWebController.java
@@ -1,12 +1,14 @@
 package hu.hjst.jobmanager.controllers.web;
 
 import hu.hjst.jobmanager.models.dtos.JobDto;
+import hu.hjst.jobmanager.models.entities.Customer;
 import hu.hjst.jobmanager.services.CustomerService;
 import hu.hjst.jobmanager.services.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -32,5 +34,11 @@ public class JobWebController {
     public String addNewJob(Model m) {
         m.addAttribute("customers", customerService.listCustomers());
         return "add-new-job-select-customer";
+    }
+
+    @PostMapping("/add-new-job-select-customer")
+    public String selectCustomer(Long id){
+        System.out.println(id);
+        return "redirect:/jobs-home";
     }
 }
