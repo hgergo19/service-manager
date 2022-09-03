63845657727c0ce3b8ca9895e32b6e9be5b2986b
Horvath Gergoe <hgergo19@gmail.com>
2022-09-03 21:20:39 +0200

    Redirect was added for job form -> select customer

--- src/main/java/hu/hjst/jobmanager/controllers/web/JobWebController.java
+++ src/main/java/hu/hjst/jobmanager/controllers/web/JobWebController.java
@@ -1,14 +1,18 @@
 package hu.hjst.jobmanager.controllers.web;
 
+import hu.hjst.jobmanager.models.dtos.JobCreateDto;
 import hu.hjst.jobmanager.models.dtos.JobDto;
 import hu.hjst.jobmanager.models.entities.Customer;
 import hu.hjst.jobmanager.services.CustomerService;
 import hu.hjst.jobmanager.services.JobService;
+import hu.hjst.jobmanager.utils.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -31,14 +35,30 @@ public class JobWebController {
     }
 
     @GetMapping("/add-new-job")
-    public String addNewJob(Model m) {
+    public String addNewJobCustomerSelector(Model m) {
         m.addAttribute("customers", customerService.listCustomers());
         return "add-new-job-select-customer";
     }
 
     @PostMapping("/add-new-job-select-customer")
     public String selectCustomer(Long id){
+        Validator.validate(id,"ID cannot be null!");
         System.out.println(id);
-        return "redirect:/jobs-home";
+
+        //TODO search if customer exists and pass it add-new-job-deatils
+        return "redirect:add-new-job/" + id;
     }
+
+    @GetMapping("/add-new-job/{id}"  )
+    public String addNewJobForm(@PathVariable Long id, Model m) {
+        m.addAttribute("customer" , customerService.findCustomerById(id));
+        return "add-new-job-details";
+    }
+
+    @PostMapping("/add-new-job"  )
+    public String addNewJob(JobCreateDto dto) {
+       //TODO IMPL!!
+        return "redirect:jobs-home";
+    }
+
 }
