7e805829a823cb0a246a06f738052e9575f75271
Horvath Gergoe <hgergo19@gmail.com>
2022-09-06 17:22:19 +0200

    Job controller was updated.
    > Post mapping for new job was added
    > refactor in various methods

--- src/main/java/hu/hjst/jobmanager/controllers/web/JobWebController.java
+++ src/main/java/hu/hjst/jobmanager/controllers/web/JobWebController.java
@@ -1,5 +1,6 @@
 package hu.hjst.jobmanager.controllers.web;
 
+import hu.hjst.jobmanager.models.dtos.JobCreateDto;
 import hu.hjst.jobmanager.models.dtos.JobDto;
 import hu.hjst.jobmanager.models.entities.Machine;
 import hu.hjst.jobmanager.services.CustomerService;
@@ -8,7 +9,9 @@ import hu.hjst.jobmanager.services.MachineService;
 import hu.hjst.jobmanager.utils.Validator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -29,7 +32,7 @@ public class JobWebController {
     }
 
     @GetMapping("/jobs-home")
-    public String getAddNewJob(Model model){
+    public String getAddNewJob(Model model) {
         List<JobDto> activeJobs = service.findActiveJobs();
         model.addAttribute("jobs", activeJobs);
         return "jobs-home";
@@ -42,23 +45,25 @@ public class JobWebController {
     }
 
     @PostMapping("/select-customer")
-    public String selectCustomer(Long id){
-        Validator.validate(id,"ID cannot be null!");
+    public String selectCustomer(Long id) {
+        Validator.validate(id, "ID cannot be null!");
         return "redirect:add-new-job/" + id;
     }
 
-    @GetMapping("/add-new-job/{id}"  )
+    @GetMapping("/add-new-job/{id}")
     public String addNewJobForm(@PathVariable Long id, Model m) {
         List<Machine> machinesByCustomer = machineService.findMachinesByCustomer(id);
-        m.addAttribute("machines" , machinesByCustomer);
-        m.addAttribute("customer" , customerService.findCustomerById(id));
+        m.addAttribute("machines", machinesByCustomer);
+        m.addAttribute("customer", customerService.findCustomerById(id));
         return "add-new-job-details";
     }
 
-//    @PostMapping("/add-new-job/{id}")
-//    public String addNewJob(@PathVariable Long id, Model model, JobCreateDto dto) {
-//       model.addAttribute("machine" , machineService.findMachinesByCustomer(id) );
-//        return "redirect:jobs-home";
-//    }
+    @PostMapping("/add-new-job")
+    public String addNewJob(JobCreateDto dto) {
+        Validator.validate(dto, "Body cannot be null!");
+        System.out.println(dto);
+        service.newJob(dto);
+        return "redirect:jobs-home";
+    }
 
 }
