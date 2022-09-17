6215e83c6d320d16559aeb9d1397c2b1fac64612
Horvath Gergoe <hgergo19@gmail.com>
2022-09-17 16:10:31 +0200

    Status enum was added.

--- src/main/java/hu/hjst/jobmanager/models/enums/Status.java
+++ src/main/java/hu/hjst/jobmanager/models/enums/Status.java
@@ -1,5 +1,28 @@
 package hu.hjst.jobmanager.models.enums;
 
 public enum Status {
-    FOLYMATBAN, SZÁMLÁZANDÓ, SZÁMLÁZVA, LEZÁRVA,
+    IN_PROGRESS("Folyamatban"),
+    INVOICE_ABLE("Számlázható"),
+    INVOICED("Számlázva"),
+    CLOSED("Lezárva");
+
+    private String text;
+
+    Status (String text){
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public static Status fromString(String text) {
+        for (Status s : Status.values()) {
+            if (s.text.equalsIgnoreCase(text)) {
+                return s;
+            }
+        }
+        return null;
+    }
+
 }
