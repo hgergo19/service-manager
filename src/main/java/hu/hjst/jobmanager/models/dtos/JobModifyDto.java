8053193b85cd12d1fd21e8a4c3569a85f4a7c306
Horvath Gergoe <hgergo19@gmail.com>
2022-10-10 17:33:45 +0200

    DaeTimeFormat was added.

--- src/main/java/hu/hjst/jobmanager/models/dtos/JobModifyDto.java
+++ src/main/java/hu/hjst/jobmanager/models/dtos/JobModifyDto.java
@@ -1,12 +1,14 @@
 package hu.hjst.jobmanager.models.dtos;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Data
 public class JobModifyDto {
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     private String status;
     private String invoiceNumber;
